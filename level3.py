83435460bc75ae26bba74eaf09f6b600de896cdc
thmmahendra <thmmahendra@gmail.com>
2026-09-11 22:27:56 +0545

    Initial Commit

--- level3.py
+++ level3.py
@@ -5,7 +5,7 @@
     # Traverses the string.
     # Counts the characters manually.
     # Prints the final count.
-
+'''
 words = input("Enter the words: ")
 
 char_count = 0
@@ -14,7 +14,7 @@ for char in words:
     char_count = char_count + 1
 
 print("You entered: ", words,
-      "\nNumber of Characters: ", char_count)
+      "\n Number of Characters: ", char_count)
 
 
 # Challenge #2
@@ -36,5 +36,33 @@ for char in words:
         char_count = char_count + 1
 
 print(character, "appeared: ", char_count, "times")
+
+'''
+# Challenge #3
+# Reverse String
+# Create a program that:
+
+    # Gets a word from the user.
+    # Starts at the last index.
+    # Moves backward.
+    # Builds a new string.
+    # Prints the reversed string.
+
+    # You can use a while loop for this challenge.
+
+words = input("Enter a words: ")
+
+reversed_words = ""
+
+index = len(words) -1
+
+while index >= 0:
+    reversed_words = reversed_words + words[index]
+    index = index - 1
+
+print(reversed_words)
+
+
+
 
 
