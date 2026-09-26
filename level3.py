4e9fb5ea7b18dd0ac77fd50799d8157327ec18d3
thmmahendra <thmmahendra@gmail.com>
2026-09-26 19:43:41 +0545

    Initial Commit

--- level3.py
+++ level3.py
@@ -1124,7 +1124,7 @@ if third_most_frequent_word == "":
 else:
     print("Third most frequent word: ", third_most_frequent_word)
     print("Third count: ", third_highest_count)
-'''
+
 
 # Challenge #38
 # Top three Least Frequent Words
@@ -1187,6 +1187,45 @@ if third_least_frequent_word == "":
 else:
     print("Third least frequent word: ", third_least_frequent_word)
     print("Third lowest count:", third_lowest_count)
+'''
+
+# Challenge #39
+# Most and Least Frequency word summary
+
+sentence = input("Enter a sentence: ").lower()
+
+words = sentence.split()
+checked_words = []
+
+most_frequent = ""
+least_frequent = ""
+
+highest_count = 0
+lowest_count = float('inf')
+
+for word in words:
+    if word not in checked_words:
+        count = 0
+
+        for check in words:
+            if word == check:
+                count = count + 1
+
+        if count > highest_count:
+            highest_count = count
+            most_frequent = word
+
+        if count < lowest_count:
+            lowest_count = count
+            least_frequent = word
+
+        checked_words.append(word)
+
+print("Most frequent word: ", most_frequent)
+print("Frequency: ", highest_count)
+
+print("Least frequent word: ", least_frequent)
+print("Frequency: ", lowest_count)
 
 
             
